ac2a4ff11aada74461a44cf8c25375c8fb10fc18
vinayk7079 <vk738393@gmail.com>
2023-11-16 15:30:17 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CSS/Footer.css'
+function Footer() {
+  return (
+    <div>
+      <p className='low'>
+        <p>footer</p>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './CSS/Header.css'
+const Header = () => {
+  return (
+    <div>
+        <p className='para'><p>header</p></p>
+    </div>
+  )
+}
+
+export default Header
